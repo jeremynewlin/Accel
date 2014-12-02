445195af123ba389cb666f19e0c94e0e79042e4c
Danny Rerucha <DannyRerucha@gmail.com>
2014-12-02 12:47:07 -0500

    Fix ray/AABB intersection test.

--- Accel/Intersections.cpp
+++ Accel/Intersections.cpp
@@ -16,79 +16,34 @@ Intersections::~Intersections()
 
 ////////////////////////////////////////////////////
 // Fast ray/AABB intersection test.
-// Implementation inspired by Andrew Woo from "Graphics Gems", Academic Press, 1990.
-// http://tog.acm.org/resources/GraphicsGems/gems/RayBox.c
+// Implementation inspired by zacharmarz.
+// https://gamedev.stackexchange.com/questions/18436/most-efficient-aabb-vs-ray-collision-algorithms
 ////////////////////////////////////////////////////
 bool Intersections::AABBIntersect( boundingBox bbox, glm::vec3 ray_o, glm::vec3 ray_dir, glm::vec3 &hit_point )
 {
-	glm::vec3 minB = bbox.min;
-	glm::vec3 maxB = bbox.max;
-	bool inside = true;
-	AABBDir quadrant[3];
-	int i;
-	int whichPlane;
-	glm::vec3 maxT( 0.0f, 0.0f, 0.0f );
-	glm::vec3 candidatePlane( 0.0f, 0.0f, 0.0f );
+	glm::vec3 dirfrac( 1.0f / ray_dir.x, 1.0f / ray_dir.y, 1.0f / ray_dir.z );
 
-	// Find candidate planes.
-	// This loop can be avoided if rays cast all from the eye (assume perpsective view).
-	for ( i = 0; i < 3; ++i ) {
-		if ( ray_o[i] < minB[i] ) {
-			quadrant[i] = LEFT;
-			candidatePlane[i] = minB[i];
-			inside = false;
-		}
-		else if ( ray_o[i] > maxB[i] ) {
-			quadrant[i] = RIGHT;
-			candidatePlane[i] = maxB[i];
-			inside = false;
-		}
-		else {
-			quadrant[i] = MIDDLE;
-		}
-	}
+	float t1 = ( bbox.min.x - ray_o.x ) * dirfrac.x;
+	float t2 = ( bbox.max.x - ray_o.x ) * dirfrac.x;
+	float t3 = ( bbox.min.y - ray_o.y ) * dirfrac.y;
+	float t4 = ( bbox.max.y - ray_o.y ) * dirfrac.y;
+	float t5 = ( bbox.min.z - ray_o.z ) * dirfrac.z;
+	float t6 = ( bbox.max.z - ray_o.z ) * dirfrac.z;
 
-	// Ray origin inside bounding box.
-	if ( inside ) {
-		hit_point = ray_o;
-		return true;
-	}
+	float tmin = std::max( std::max( std::min( t1, t2 ), std::min( t3, t4 ) ), std::min( t5, t6 ) );
+	float tmax = std::min( std::min( std::max( t1, t2 ), std::max( t3, t4 ) ), std::max( t5, t6 ) );
 
-	// Compute T distances to candidate planes.
-	for ( i = 0; i < 3; ++i ) {
-		if ( quadrant[i] != MIDDLE && ray_dir[i] > TEST_EPSILON && ray_dir[i] < -TEST_EPSILON ) {
-			maxT[i] = ( candidatePlane[i] - ray_o[i]) / ray_dir[i];
-		}
-		else {
-			maxT[i] = -1.0f;
-		}
-	}
-
-	// Get largest of the maxT's for final choice of intersection.
-	whichPlane = 0;
-	for ( i = 1; i < 3; i++ ) {
-		if ( maxT[whichPlane] < maxT[i] ) {
-			whichPlane = i;
-		}
-	}
-
-	// Check final candidate actually inside box.
-	if ( maxT[whichPlane] < 0.0f ) {
+	// If tmax < 0, ray intersects AABB, but entire AABB is behing ray, so reject.
+	if ( tmax < 0.0f ) {
 		return false;
 	}
-	for ( i = 0; i < 3; i++ ) {
-		if ( whichPlane != i ) {
-			hit_point[i] = ray_o[i] + ( maxT[whichPlane] * ray_dir[i] );
-			if ( hit_point[i] < minB[i] || hit_point[i] > maxB[i] ) {
-				return false;
-			}
-		}
-		else {
-			hit_point[i] = candidatePlane[i];
-		}
+
+	// If tmin > tmax, ray does not intersect AABB.
+	if ( tmin > tmax ) {
+		return false;
 	}
 
-	// Ray hits box.
+	hit_point =  ray_o + ( tmin * ray_dir );
 	return true;
 }
 
